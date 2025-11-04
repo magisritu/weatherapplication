d0674b3e4250ab4472ad0e134d79e91d11cb48a0
magisritu <magisriturajverma@gmail.com>
2025-11-05 00:51:08 +0530

    test commit

--- AzureCoreAPI/Controllers/WeatherForecastController.cs
+++ AzureCoreAPI/Controllers/WeatherForecastController.cs
@@ -60,6 +60,7 @@ namespace AzureCoreAPI.Controllers
         public async Task<IActionResult> UpdateWeatherImage([FromForm] UpdateWeatherImage model)
         {
             //string pictureUrl = null;
+            // Test commit
             var functionUrl = _config.GetSection("AzureFunction").GetSection("WeatherImageUpload").Value;
             // Call Azure fucntion to upload image
 
